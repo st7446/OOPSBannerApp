22e546d2e967b5b0d93e2d882d78712244fe79ac
st7446 <st7446@srmist.edu.in>
2026-02-17 09:29:19 +0530

    UC4: Implemented banner using String array and for-each loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,24 @@
 /**
- * UC3: Print OOPS in Banner Format using String.join()
+ * UC4: print OOPS in Banner Format using array
  *
- * @author hk9981
+ * @author sai-baibhab
  * @version 1.0
  */
-public class UC3PrintBanner {
+public class UC4printBanner {
 
     public static void main(String[] args) {
+        String[] lines = new String[6];
 
-        // OOPS Banner using String.join() (UC3)
-        System.out.println(String.join(" ", "  ***  ", "  ***  ", " ****  ", " ***** "));
-        System.out.println(String.join(" ", " *   * ", " *   * ", " *   * ", " *     "));
-        System.out.println(String.join(" ", "*     *", "*     *", " ****  ", " ****  "));
-        System.out.println(String.join(" ", "*     *", "*     *", " *     ", "     * "));
-        System.out.println(String.join(" ", " *   * ", " *   * ", " *     ", " *   * "));
-        System.out.println(String.join(" ", "  ***  ", "  ***  ", " *     ", " ***** "));
+        // OOPS Banner using array (UC4)
+        lines[0] = String.join(" ", "  ***  ", "  ***  ", " ****  ", " ***** ");
+        lines[1] = String.join(" ", " *   * ", " *   * ", " *   * ", " *     ");
+        lines[2] = String.join(" ", "*     *", "*     *", " ****  ", " ****  ");
+        lines[3] = String.join(" ", "*     *", "*     *", " *     ", "     * ");
+        lines[4] = String.join(" ", " *   * ", " *   * ", " *     ", " *   * ");
+        lines[5] = String.join(" ", "  ***  ", "  ***  ", " *     ", " ***** ");
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
