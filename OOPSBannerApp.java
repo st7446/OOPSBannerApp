bb684116e7545708caea0bcda7ac981f0b89f8fa
st7446 <st7446@srmist.edu.in>
2026-03-02 08:44:33 +0530

    initial commit - UC6 OOPS Banner App With Modular methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,54 @@
 public class OOPSBannerApp {
+    public static String[] getOPattern() {
+        return new String[]{
+                "  *****  ",
+                " *     * ",
+                "*       *",
+                "*       *",
+                "*       *",
+                " *     * ",
+                "  *****  "
+        };
+    }
+
+    public static String[] getPPattern() {
+        return new String[]{
+                " ******  ",
+                " *     * ",
+                " *     * ",
+                " ******  ",
+                " *       ",
+                " *       ",
+                " *       "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[]{
+                "  *****  ",
+                " *     * ",
+                " *       ",
+                "  *****  ",
+                "        *",
+                " *     * ",
+                "  *****  "
+        };
+    }
 
     public static void main(String[] args) {
 
-        String[] banner = {
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-            String.join("   ", " ***** ", " ***** ", " ****** ", " ***** "),
-            String.join("   ", "*     *", "*     *", "*      *", "*     "),
-            String.join("   ", "*     *", "*     *", "*      *", "*     "),
-            String.join("   ", "*     *", "*     *", " ****** ", " ***** "),
-            String.join("   ", "*     *", "*     *", "*       ", "     * "),
-            String.join("   ", "*     *", "*     *", "*       ", "*    * "),
-            String.join("   ", " ***** ", " ***** ", "*       ", " ****  ")
 
-        };
-
-        for (String line : banner) {
-            System.out.println(line);
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                    oPattern[i] + "  " +
+                    oPattern[i] + "  " +
+                    pPattern[i] + "  " +
+                    sPattern[i]
+            );
         }
     }
 }
