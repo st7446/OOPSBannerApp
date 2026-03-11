be3b144db0c80e94675c766f7ec9cd3a0eebc483
st7446 <st7446@srmist.edu.in>
2026-03-11 08:44:08 +0530

    UC8: Implement HashMap for efficient character pattern management and StringBuilder for rendering

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,71 +1,54 @@
 public class OOPSBannerApp {
 
-
-    static class CharacterPatternMap {
-        private final Character character;
-        private final String[] pattern;
-
-        public CharacterPatternMap(Character character, String[] pattern) {
-            this.character = character;
-            this.pattern = pattern;
-        }
-
-        public Character getCharacter() {
-            return character;
-        }
-
-        public String[] getPattern() {
-            return pattern;
-        }
+    public static String[] getOPattern() {
+        return new String[]{
+                String.join("", "  ***  "),
+                String.join("", " *   * "),
+                String.join("", "*     *"),
+                String.join("", "*     *"),
+                String.join("", "*     *"),
+                String.join("", " *   * "),
+                String.join("", "  ***  ")
+        };
     }
 
-    
-    public static CharacterPatternMap[] createCharacterPatternMaps() {
-        CharacterPatternMap[] maps = new CharacterPatternMap[4];
-
-        maps[0] = new CharacterPatternMap('O', new String[]{
-            "  ***  ", "**   **", "**   **", "**   **", "**   **", "**   **", "  *** "
-        });
-        maps[1] = new CharacterPatternMap('P', new String[]{
-            "****** ", "**   **", "**   **", "****** ", "** ", "** ", "  ** "
-        });
-        maps[2] = new CharacterPatternMap('S', new String[]{
-            " ***** ", "**   **", "** ", " ***** ", "         **", "    **   **", "     ***** "
-        });
-        maps[3] = new CharacterPatternMap(' ', new String[]{
-            "       ", "       ", "       ", "       ", "       ", "       ", "       "
-        });
-
-        return maps;
+    public static String[] getPPattern() {
+        return new String[]{
+                String.join("", "*****  "),
+                String.join("", "*    * "),
+                String.join("", "*    * "),
+                String.join("", "*****  "),
+                String.join("", "*      "),
+                String.join("", "*      "),
+                String.join("", "*      ")
+        };
     }
 
-    
-    public static String[] getCharacterPattern(char ch, CharacterPatternMap[] charMaps) {
-        for (CharacterPatternMap map : charMaps) {
-            if (map.getCharacter() == Character.toUpperCase(ch)) {
-                return map.getPattern();
-            }
-        }
-        
-        return charMaps[3].getPattern();
-    }
-
-    public static void printMessage(String message, CharacterPatternMap[] charMaps) {
-        int lines = 7; 
-        for (int i = 0; i < lines; i++) {
-            StringBuilder sb = new StringBuilder();
-            for (char ch : message.toCharArray()) {
-                String[] pattern = getCharacterPattern(ch, charMaps);
-                sb.append(pattern[i]).append("  "); 
-            }
-            System.out.println(sb.toString());
-        }
+    public static String[] getSPattern() {
+        return new String[]{
+                String.join("", " ***** "),
+                String.join("", "*      "),
+                String.join("", "*      "),
+                String.join("", " ***** "),
+                String.join("", "      *"),
+                String.join("", "      *"),
+                String.join("", " ***** ")
+        };
     }
 
     public static void main(String[] args) {
-        CharacterPatternMap[] charMaps = createCharacterPatternMaps();
 
-        String message = "OOPS";
-        printMessage(message, charMaps);
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                    oPattern[i] + "  " +
+                    oPattern[i] + "  " +
+                    pPattern[i] + "  " +
+                    sPattern[i]
+            );
+        }
     }
 }
